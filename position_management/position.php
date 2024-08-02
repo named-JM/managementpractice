9b30e12af84642b658eff20e9b5a70d76210e2bb
Echel Cags <joannacaguco@gmail.com>
2024-08-02 10:45:50 +0800

    added position management file

---
+++ position_management/position.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>POSITION MANGEMENT</h1>
+    <form action="position.php" method="post">
+        
+        <!-- NEW POSITION/OCCUPATION -->
+        <label for="new_position">New Position</label>
+        <input type="text" id="pos_name" name="pos_name">
+        <br>
+        <!-- POSITION NAME/ NAME OF THE EMPLOYER IN EMPLOYEMNT -->
+
+
+
+        <!-- CONTRACTS / KUNG ANON YUNG MGA COMPENSATION -->
+        <!-- dropdown pos_employment which is yung contract doon sa employment kukunin -->
+        <label for="pos_employment">Contracts</label>
+        
+
+        <!-- SALARY -->
+        <label for="salary">Salary</label>
+        <input type="number" id="pos_salary">
+
+        <!-- BENEFITS -->
+        <!-- dropdown then may plus icon for adding another dropdown and yung ma value
+        nandoon sa benefits management -->
+
+        
+
+    </form>
+</body>
+</html>
