54ca1ce86b8aeeaae5067f0889ff4c5fe004b7d4
Echel Cags <joannacaguco@gmail.com>
2024-08-07 15:45:33 +0800

    add labels and input in employee

--- employee_management/add_employee.php
+++ employee_management/add_employee.php
@@ -1,5 +1,5 @@
 <?php
-
+    include "../db_connection.php";
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +10,48 @@
     <title>Employee Management</title>
 </head>
 <body>
+    <h1>Employee Management</h1>
+    <!-- EMP FIRSTNAME -->
+    <form action="add_employee.php" method="post">
+    <label for="emp_fname">First Name</label>
+    <input type="text" id="emp_fname" name="emp_fname" required>
+    <br><br>
+
+    <!-- EMP MIDDLE NAME -->
+
+    <label for="emp_mname">Middle Name</label>
+    <input type="text" id="emp_mname" name="emp_mname" required>
+    <br><br>
+    <!-- EMP LAST NAME -->
+    <label for="emp_lname">Last Name</label>
+    <input type="text" id="emp_lname" name="emp_lname" required>
+    <br><br>
+
+    <!-- EMP POSITION  TAWAGAIN SA POSITION TABLE AS A NUMBER NA LANG-->
+    <label for="emp_position">Position</label>
+    <input type="text" id="emp_position" name="emp_position" required>
+    <br><br>
+    <!-- EMP EMAIL -->
+    <label for="emp_email">Email</label>
+    <input type="text" id="emp_email" name="emp_email" required>
+    <br><br>
+    <!-- EMP NUMBER -->
+    <label for="emp_number">Phone Number</label>
+    <input type="text" id="emp_number" name="emp_number" required>
+    <br><br>
+
+    <!-- EMP ZIP CODE PLACE -->
+    <label for="emp_zip">Zip Code</label>
+    <input type="text" id="emp_zip" name="emp_zip" required>
+    <br><br>
+
+    <!-- EMP FILE/MOVE UPLOAD, WAG NA SA DATABASE -->
+    <label for="emp_file">Upload CV/Resume</label>
+    <input type="text" id="emp_file" name="emp_file" required>
+    <br><br>
+    </form>
+
+    
     
 </body>
 </html>
